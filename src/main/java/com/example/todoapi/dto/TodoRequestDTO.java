aa7eb6aa2f1272af5d6813fdf6c0a9ab86399182
alsmk <alhel444@gmail.com>
2025-12-03 23:21:57 +0600

    A documment created named todos

---
+++ src/main/java/com/example/todoapi/dto/TodoRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.todoapi.dto;
+
+public class TodoRequestDTO {
+}
--- src/main/java/com/example/todoapi/model/Todo.java
+++ src/main/java/com/example/todoapi/model/Todo.java
@@ -1,5 +1,34 @@
 package com.example.todoapi.model;
 
-public class Todo {
+import jakarta.annotation.Priority;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "todos")
+public class Todo {
+   @Id
+   private String id;
+   private String title;
+   private String description;
+   private boolean completed;
+   private Priority priority;
+   @CreatedDate
+   private LocalDateTime createdAt;
+   @LastModifiedDate
+   private LocalDateTime updatedAt;
+   public enum  Priority {
+      HIGH,
+      MEDIUM,
+      LOW
+   }
 }
